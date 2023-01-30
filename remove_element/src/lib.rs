863fe190002ad6200ec7b79e4c82149f49a18151
SunsJay <sunsjay0806@gmail.com>
2023-01-30 17:29:06 +0800

    feat: two point method

--- remove_element/src/lib.rs
+++ remove_element/src/lib.rs
@@ -1,7 +1,24 @@
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> usize {
-        nums.retain(|&x| x != val ) ;
+        nums.retain(|&x| x != val);
         nums.len()
+    }
+
+    pub fn remove_element_a(nums: &mut Vec<i32>, val: i32) -> usize {
+        if nums.len() == 0 {
+            return 0;
+        }
+        let mut slow_point = 0;
+
+
+        for pos in 0..nums.len() {
+            if nums[pos] != val {
+                nums[slow_point] = nums[pos];
+                slow_point += 1;
+            }
+        }
+
+        slow_point
     }
 }
 
