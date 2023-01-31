da8bed098057384a9264d9f3e76897296b2731b3
SunsJay <sunsjay0806@gmail.com>
2023-01-31 09:33:03 +0800

    feat: two_num_hash

--- two_num/src/lib.rs
+++ two_num/src/lib.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 impl Solution {
+    // 暴力遍历
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         for i in 0..nums.len() {
             for j in i + 1..nums.len() {
@@ -9,4 +12,21 @@ impl Solution {
         }
         nums
     }
+
+    // 哈希表法
+    pub fn two_sum_hash(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, usize> = HashMap::new();
+        for i in 0..nums.len() {
+            match map.get(&(target - nums[i])) {
+                None => {
+                    map.insert(nums[i], i);
+                }
+                Some(&v) => {
+                    return vec![v as i32, i as i32]
+                }
+            }
+        }
+        nums
+    }
 }
+
