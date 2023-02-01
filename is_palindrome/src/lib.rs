e46b0f980f86ffcdfb24584bf034d9c0029eb1e0
SunsJay <sunsjay0806@gmail.com>
2023-02-01 09:15:44 +0800

    fix: error

--- is_palindrome/src/lib.rs
+++ is_palindrome/src/lib.rs
@@ -1,12 +1,12 @@
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
-        let mut x = x;
+        let mut y = x;
         let mut reverted_number = 0;
         // 所有负数和十的倍数不可能为回文
-        if x >= 0 && x / 10 != 0 {
-            while x != 0 {
-                reverted_number = reverted_number * 10 + (x % 10);
-                x = x / 10;
+        if y > 0 && y / 10 != 0 {
+            while y != 0 {
+                reverted_number = reverted_number * 10 + (y % 10);
+                y = y / 10;
             }
 
             if reverted_number == x {
