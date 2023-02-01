1c7cc0db592ceabd104a539ecd3626a00ccc5f65
SunsJay <sunsjay0806@gmail.com>
2023-02-01 09:12:24 +0800

    fix: equal

--- is_palindrome/src/lib.rs
+++ is_palindrome/src/lib.rs
@@ -4,17 +4,13 @@ impl Solution {
         let mut reverted_number = 0;
         // 所有负数和十的倍数不可能为回文
         if x >= 0 && x % 10 != 0 {
-            while x / 10 != 0 {
+            while x == 0 {
                 reverted_number = reverted_number * 10 + (x % 10);
                 x = x / 10;
+            }
 
-                if reverted_number == x {
-                    return true;
-                }
-
-                if reverted_number > x {
-                    break;
-                }
+            if reverted_number == x {
+                return  true;
             }
         }
 
