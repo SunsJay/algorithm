131a88eafef1d3cbd92e83112c00acc588a2119c
SunsJay <sunsjay0806@gmail.com>
2023-02-01 09:18:50 +0800

    fix: fix

--- is_palindrome/src/lib.rs
+++ is_palindrome/src/lib.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
         let mut y = x;
         let mut reverted_number = 0;
-        // 所有负数和十的倍数不可能为回文
+        // 所有负数不可能为回文
         if y >= 0 {
             while y != 0 {
                 reverted_number = reverted_number * 10 + (y % 10);
