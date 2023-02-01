36999a3cb108fbc032b16fa235d11bf58fd37f81
SunsJay <sunsjay0806@gmail.com>
2023-02-01 09:10:41 +0800

    feat: is_palindrome

--- is_palindrome/src/lib.rs
+++ is_palindrome/src/lib.rs
@@ -1,12 +1,23 @@
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
+        let mut x = x;
+        let mut reverted_number = 0;
         // 所有负数和十的倍数不可能为回文
-        if x >= 0 && x % 10 != 0  {
+        if x >= 0 && x % 10 != 0 {
+            while x / 10 != 0 {
+                reverted_number = reverted_number * 10 + (x % 10);
+                x = x / 10;
 
+                if reverted_number == x {
+                    return true;
+                }
 
+                if reverted_number > x {
+                    break;
+                }
+            }
         }
 
         false
-
     }
 }
