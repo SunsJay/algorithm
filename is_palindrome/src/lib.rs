4bfa583bc2c199c703ebfb206f2ded33213d642b
SunsJay <sunsjay0806@gmail.com>
2023-02-01 09:17:11 +0800

    feat: is_pailndrome

--- is_palindrome/src/lib.rs
+++ is_palindrome/src/lib.rs
@@ -3,14 +3,14 @@ impl Solution {
         let mut y = x;
         let mut reverted_number = 0;
         // 所有负数和十的倍数不可能为回文
-        if y > 0 && y / 10 != 0 {
+        if y >= 0 {
             while y != 0 {
                 reverted_number = reverted_number * 10 + (y % 10);
                 y = y / 10;
             }
 
             if reverted_number == x {
-                return  true;
+                return true;
             }
         }
 
