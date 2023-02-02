78d1d246670c812e78cbc1504e5570c4b1f7d6f2
SunsJay <sunsjay0806@gmail.com>
2023-02-02 21:11:31 +0800

    feat: two points

--- remove-duplicates-from-sorted-array/src/lib.rs
+++ remove-duplicates-from-sorted-array/src/lib.rs
@@ -3,4 +3,19 @@ impl Solution {
         nums.dedup();
         nums.len() as i32
     }
+
+    // 双指针法
+    pub fn remove_duplicates_two_points(nums: &mut Vec<i32>) -> i32 {
+        let mut j = 0;
+        for i in 1..nums.len() {
+            if nums[i] != nums[j] {
+                nums[j + 1] = nums[i];
+                j += 1;
+            }
+        }
+
+        j as i32  + 1
+    }
+
+
 }
