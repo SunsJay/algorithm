636ca284c5b7c9e38ac8456c8decd4892d929183
SunsJay <sunsjay0806@gmail.com>
2023-02-03 09:08:18 +0800

    feat: merge

--- merge-two-sorted-lists/src/lib.rs
+++ merge-two-sorted-lists/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+impl Solution {
+    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+      match (list1, list2)  {
+        (None,None) => None,
+        (None, l2) => l2,
+        (l1, None) => l1,
+        (Some(mut l1), Some(mut l2)) => {
+          if l1.val <= l2.val {
+            l1.next = Self::merge_two_lists(l1.next, Some(l2));
+            Some(l1)
+          } else {
+            l2.next = Self::merge_two_lists(Some(l1), l2.next);
+            Some(l2)
+          }
+        }
+      }
     }
 }
