41f1eed23324f5cfc49ed7f5a04b60354b325464
SunsJay <sunsjay0806@gmail.com>
2023-02-04 17:13:59 +0800

    fix: loop

--- plus-one/src/lib.rs
+++ plus-one/src/lib.rs
@@ -1,19 +1,16 @@
 impl Solution {
     pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-        // if digits.len() == 1 && digits[0] == 0 {
-        //     return vec![1];
-        // }
         let mut new_digits: Vec<i32> = digits.clone();
-        for i in (digits.len() - 1)..=0 {
+        let mut i = digits.len() - 1;
+        while i >= 0 {
             if digits[i] != 9 {
                 new_digits[i] = digits[i] + 1;
-                println!("{} - {}", digits[i] , new_digits[i]);
                 return new_digits;
             } else {
                 new_digits[i] = 0;
             }
+            i -= 1;
         }
-
         if new_digits[0] == 0 {
             new_digits.insert(0, 1);
         }
@@ -22,15 +19,15 @@ impl Solution {
     }
 }
 
-struct Solution{}
+struct Solution {}
 
 #[cfg(test)]
-mod test{
+mod test {
     use crate::Solution;
 
     #[test]
     fn test() {
-        let a = vec![1,2,3];
+        let a = vec![1, 9, 9];
         let b = Solution::plus_one(a);
         println!("{:?}", b);
     }
