d6068713a326bd5170f39dfb7db2c69b9e85c4b8
SunsJay <sunsjay0806@gmail.com>
2023-02-05 17:38:34 +0800

    feat: search

--- search-insert-position/src/lib.rs
+++ search-insert-position/src/lib.rs
@@ -1,5 +1,10 @@
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-
+        for i in 0..nums.len() {
+            if target < nums[i + 1] && target >= nums[i] {
+                return i as i32
+            }
+        }
+        0
     }
 }
