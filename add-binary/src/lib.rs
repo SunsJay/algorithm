85b10a97afce40350b7b8737728a24b2f0c7241b
SunsJay <sunsjay0806@gmail.com>
2023-02-06 21:34:14 +0800

    style: remove

--- add-binary/src/lib.rs
+++ add-binary/src/lib.rs
@@ -4,13 +4,4 @@ impl Solution {
         format!("{:b}", total)
     }
 }
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
