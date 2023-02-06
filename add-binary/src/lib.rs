f00ce3f29f6f395f768fff9b085264c9574bad70
SunsJay <sunsjay0806@gmail.com>
2023-02-06 21:45:03 +0800

    feat: finish

--- add-binary/src/lib.rs
+++ add-binary/src/lib.rs
@@ -1,10 +1,11 @@
 impl Solution {
     pub fn add_binary(a: String, b: String) -> String {
-        let total = 1;
-        println!("{}", a.parse::<i32>().unwrap());
-        format!("{:b}", total)
+        let ia = i128::from_str_radix(&a, 2).unwrap();
+        let ib = i128::from_str_radix(&b, 2).unwrap();
+        format!("{:b}", (ia+ib)).to_string()
     }
 }
+
 
 struct Solution {}
 
