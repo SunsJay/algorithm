4f840d25877b04fe830b69ad3b7358cd773d2057
SunsJay <sunsjay0806@gmail.com>
2023-02-06 21:33:42 +0800

    feat: finish

--- add-binary/src/lib.rs
+++ add-binary/src/lib.rs
@@ -1,7 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+impl Solution {
+    pub fn add_binary(a: String, b: String) -> String {
+        let total: i32 = a.parse::<i32>().unwrap() + b.parse::<i32>().unwrap();
+        format!("{:b}", total)
+    }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
