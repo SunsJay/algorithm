e4588fa1093f51fa9f0a539fa21b107c46649210
SunsJay <sunsjay0806@gmail.com>
2023-02-06 21:40:14 +0800

    feat: test

--- add-binary/src/lib.rs
+++ add-binary/src/lib.rs
@@ -1,7 +1,21 @@
 impl Solution {
     pub fn add_binary(a: String, b: String) -> String {
-        let total: i32 = a.parse::<i32>().unwrap() + b.parse::<i32>().unwrap();
+        let total = 1;
+        println!("{}", a.parse::<i32>().unwrap());
         format!("{:b}", total)
     }
 }
 
+struct Solution {}
+
+#[cfg(test)]
+mod test {
+    use crate::Solution;
+
+    #[test]
+    fn test_one() {
+        use super::*;
+        let a = Solution::add_binary("11".to_string(), "1".to_string());
+        println!("{}", a);
+    }
+}
