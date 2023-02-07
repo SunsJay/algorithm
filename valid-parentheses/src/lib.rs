ea9f12947dd9e3c7322fe08e672bca07885a9c32
SunsJay <sunsjay0806@gmail.com>
2023-02-07 21:31:54 +0800

    feat: finish

--- valid-parentheses/src/lib.rs
+++ valid-parentheses/src/lib.rs
@@ -1,8 +1,18 @@
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-
+        if s.len() & 1 == 1 { return false; }
+        let mut v = vec![];
+        for c in s.into_bytes() {
+            match c {
+                b'(' | b'[' | b'{' => v.push(c + (1 << (c & 1))),
+                _ => if Some(c) != v.pop() { return false }
+            }
+        }
+        v.is_empty()
     }
 }
+
+
 
 #[cfg(test)]
 mod tests {
